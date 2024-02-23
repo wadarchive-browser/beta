58d48b2608cf5a1c82f8d15f189a342f84d5d8e8
uwx <uwx@users.noreply.github.com>
2024-02-23 04:57:20 -0300

    wtf

--- svelte.config.js
+++ svelte.config.js
@@ -4,27 +4,22 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = /** @satisfies {import('@sveltejs/kit').Config} */ ({
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter({
+            fallback: '404.html'
+        }),
 
-		prerender: {
-			// @ts-expect-error
-			entries: ['/', '/wad-index', '/wad-index/digit', '/search', '/googlesearch', '/wad', ...'0123456789abcdefghijklmnopqrstuvwxyz'.split('').map(e => '/wad-index/' + e)]
-		},
-
-		paths: {
-			base: '/beta'
-		},
-	}
+        paths: {
+            base: '/beta'
+        },
+    }
 });
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,39 @@ export default defineConfig({
     worker: {
 		plugins: () => [sveltekit()],
         format: 'es'
+    },
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                arguments: true,
+                booleans: true,
+                hoist_funs: true,
+                join_vars: true,
+                keep_classnames: false,
+                keep_fnames: false,
+                keep_infinity: true,
+                module: true,
+                unsafe: true,
+                toplevel: false,
+                unsafe_methods: true,
+                unsafe_proto: true,
+                unsafe_regexp: true,
+                // reduce_funcs: false,
+
+                // unsafe_passes: true,
+                unsafe_arrows: true,
+                // unsafe_comps: true,
+                unsafe_Function: true,
+                // unsafe_math: true,
+                unsafe_symbols: true,
+
+                unused: true,
+
+                passes: 6,
+            },
+            sourceMap: true,
+            mangle: false
+        }
     }
 });
