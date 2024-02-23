8cc54a92d551b8ed173db291b5589643dfac513b
uwx <uwx@users.noreply.github.com>
2024-02-23 01:42:24 -0300

    Set base url to /beta for now

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '404.html'
-		})
+		}),
+
+		base: '/beta'
 	}
 };
 
