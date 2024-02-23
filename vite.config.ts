35f639fda1141c51eb5db8a9dd5d0228fb151d7c
uwx <uwx@users.noreply.github.com>
2024-02-23 19:28:58 -0300

    beep

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import terser from '@rollup/plugin-terser';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    ssr: {
-        noExternal: ['@popperjs/core']
-    },
-    worker: {
-		// plugins: () => [sveltekit()],
-        format: 'es'
-    },
-    build: {
-        minify: 'terser',
-        terserOptions: {
+    plugins: [
+        sveltekit(),
+        terser({
             compress: {
                 arguments: true,
                 booleans: true,
@@ -45,6 +38,52 @@ export default defineConfig({
                 module: true,
                 toplevel: true,
             }
-        }
+        }),
+    ],
+    ssr: {
+        noExternal: ['@popperjs/core']
+    },
+    worker: {
+        plugins: () => [
+            sveltekit(),
+            terser({
+                compress: {
+                    arguments: true,
+                    booleans: true,
+                    hoist_funs: true,
+                    join_vars: true,
+                    keep_classnames: false,
+                    keep_fnames: false,
+                    keep_infinity: true,
+                    module: true,
+                    unsafe: true,
+                    toplevel: true,
+                    unsafe_methods: true,
+                    unsafe_proto: true,
+                    unsafe_regexp: true,
+                    // reduce_funcs: false,
+
+                    // unsafe_passes: true,
+                    unsafe_arrows: true,
+                    // unsafe_comps: true,
+                    unsafe_Function: true,
+                    // unsafe_math: true,
+                    unsafe_symbols: true,
+
+                    unused: true,
+
+                    passes: 6,
+                },
+                sourceMap: true,
+                mangle: {
+                    module: true,
+                    toplevel: true,
+                }
+            }),
+        ],
+        format: 'es'
+    },
+    build: {
+        minify: 'esbuild',
     }
 });
