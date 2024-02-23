f2f14a74312127cce0abf418c746fd616030779d
uwx <uwx@users.noreply.github.com>
2024-02-23 01:42:15 -0300

    Improve /index/[initial] styling

--- src/routes/index/[initial]/+page.svelte
+++ src/routes/index/[initial]/+page.svelte
@@ -9,8 +9,9 @@
     import { base } from "$app/paths";
     import { Wad } from "../../../util/msgpack-models";
     import Pagination from "../../../components/Pagination.svelte";
+    import { trimToLength } from "../../../util";
 
-    const displayInitial = $page.params.initial === 'digit' ? '#' : $page.params.initial;
+    const displayInitial = $page.params.initial === 'digit' ? '#' : $page.params.initial.toUpperCase();
 
     async function queryInitial(): Promise<Wad[]> {
         const decodedMessage = await fetchAndParseZstd(`${base}/wad-index/${$page.params.initial}.msg.zstd`) as unknown[][];
@@ -20,9 +21,18 @@
 </script>
 
 <style>
-    .resultlink {
+    :global(.resultlink) {
         color: inherit;
         text-decoration: none;
+    }
+
+    :global(.thumb-col) {
+        width: 28%;
+        padding-right: 0;
+    }
+
+    :global(.thumb-col + .col) {
+        padding-left: 0;
     }
 </style>
 
@@ -34,22 +44,19 @@
 
 <!--<div class="gcse-search" />-->
 
+<h2>{displayInitial}</h2>
 {#await queryInitial()}
-    <h2>{displayInitial}</h2>
+    <Jumper size="60" color="var(--bs-code-color)" unit="px" duration="1.5s" />
 {:then wads}
-    <h2>{displayInitial} - {wads.length} entries</h2>
-
     <Pagination rows={wads} perPage={50} totalShownPages={3} let:row>
         {@const wad = row}
         <a href="/wad?id={wad.IdSmall}" class="resultlink">
             <Card class="mb-3">
                 <Row>
-                    {#if wad.MainScreenshot}
-                    <Col xs="3">
-                        <Image fluid thumbnail alt="Screenshot for {wad.Title}" src={wad.MainScreenshot}></Image>
+                    <Col xs="3" class="thumb-col">
+                        <Image fluid alt="Screenshot for {wad.Title}" class="thumb" src={wad.MainScreenshot ?? 'https://placehold.co/800x600/111/EEE/png?text=No%20image'}></Image>
                     </Col>
-                    {/if}
-                    <Col xs="auto">
+                    <Col>
                         <CardHeader>
                             {#if wad.IdealTitle}
                                 <CardTitle>{wad.IdealTitle} ({wad.IdealFilename})</CardTitle>
@@ -59,12 +66,26 @@
                         </CardHeader>
                         <CardBody>
                             <CardText>
-                                {#if wad.FallbackNames.length}
-                                Alternative titles: {wad.FallbackNames.slice(1).join('; ')}<br>
+                                {#if wad.FormattedDescription}
+                                    <p>{trimToLength(250, wad.FormattedDescription)}</p>
                                 {/if}
-                                {#if wad.FallbackFilenames.length}
-                                Alternative file names:
-                                {#each wad.FallbackFilenames.slice(1) as filename, i}{i != 0 ? ";" : ""} <code>{filename}</code>{/each}<br>
+                                {#if wad.FallbackNames.length > 0}
+                                    {#if wad.IdealTitle != wad.FallbackNames[0]}
+                                        Alternative titles: {wad.FallbackNames.join('; ')}<br>
+                                    {:else}
+                                        {#if wad.FallbackNames.length > 1}
+                                            Alternative titles: {wad.FallbackNames.slice(1).join('; ')}<br>
+                                        {/if}
+                                    {/if}
+                                {/if}
+                                {#if wad.FallbackFilenames.length > 0}
+                                    {#if wad.IdealFilename != wad.FallbackFilenames[0]}
+                                        Alternative file names: {#each wad.FallbackFilenames as filename, i}{i != 0 ? ";" : ""} <code>{filename}</code>{/each}<br>
+                                    {:else}
+                                        {#if wad.FallbackFilenames.length > 1}
+                                            Alternative file names: {#each wad.FallbackFilenames.slice(1) as filename, i}{i != 0 ? ";" : ""} <code>{filename}</code>{/each}<br>
+                                        {/if}
+                                    {/if}
                                 {/if}
                                 ID: <code>{wad.IdSmall}</code><br>
                                 SHA-1: <code>{wad.Id}</code><br>
