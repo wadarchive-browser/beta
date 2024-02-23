544138189dd8e260648c7b30b1af9999b13b6a66
uwx <uwx@users.noreply.github.com>
2024-02-23 01:47:47 -0300

    Fix base

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @satisfies {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
@@ -15,7 +15,9 @@ const config = {
 			fallback: '404.html'
 		}),
 
-		base: '/beta'
+		paths: {
+			base: '/beta'
+		},
 	}
 };
 
